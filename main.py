c25b6f70510ee7734317379f1929b2b310a10a6c
rodrigofelixf <rodrigofelixferreira@hotmail.com>
2024-11-28 15:36:30 -0300

    expoe na  aplicacao o endpoint usuario

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.controller.VazamentoController import router as api_router
+from app.controller.UsuarioController import routerusuarios as api_router_usuarios
 
 app = FastAPI()
 
 
 app.include_router(api_router, prefix="/v1/api")
+app.include_router(api_router_usuarios, prefix="/v1/api")
