813ee7d1c4eeb51d235b315df0f74105454d6dc0
Sebastyan <sebastyanfdesouza@gmail.com>
2024-11-28 12:53:40 -0300

    Update VazamentoService.py
    Alteração do limit

--- app/service/VazamentoService.py
+++ app/service/VazamentoService.py
@@ -46,7 +46,10 @@ def get_vazamento_by_email(email: str, db: Session):
 
 
 def get_all_vazamentos(db: Session, skip: int = 0, limit: int = 100):
-  return db.query(models.Vazamento).offset(skip).limit(limit).all()
+  if limit == 0: # verefica se o parametro e igual a 0
+    return db.query(models.Vazamento).offset(skip).all()   # retorna a query sem limit
+  else:
+    return db.query(models.Vazamento).offset(skip).limit(limit).all() # a query tem limit
 
 
 def create_vazamento( vazamento: schemas.VazamentoRequest, db: Session):
