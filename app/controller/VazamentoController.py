6c33816586f391b7e1b3266ae91b8ed0eb883c22
Sebastyan <sebastyanfdesouza@gmail.com>
2024-11-28 16:32:52 -0300

    novo-endpoint
    endpoint exporvazamentos

--- app/controller/VazamentoController.py
+++ app/controller/VazamentoController.py
@@ -6,6 +6,7 @@ from app.models import models
 from app.models import schemas
 from app.db.database import SessionLocal, engine
 from sqlalchemy.orm import Session
+from typing import List
 
 models.Base.metadata.create_all(bind= engine)
 router = APIRouter()
@@ -44,7 +45,24 @@ def criar_vazamentos(vazamentos: schemas.VazamentoRequest,  db: Session= Depends
 
     vazamento= VazamentoService.create_vazamento(vazamentos, db)
 
+
     return  vazamento
+
+
+@router.get(endpointVazamento + "exporvazamentos/", response_model=List[schemas.VazamentoReponse])
+def expor_vazamentos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """
+    Endpoint para buscar todos os vazamentos de e-mails, com paginação.
+    """
+    vazamentos = VazamentoService.get_all_vazamentos(db, skip, limit) # Função do serviço para pegar os vazamentos
+
+    if not vazamentos: # Verificando se foram encontrados vazamentos
+        raise HTTPException(status_code=404, detail="Nenhum vazamento encontrado")
+
+    return vazamentos
+
+
+
 
 
 
