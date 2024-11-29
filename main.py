81f2bfe60ce914657e0e5f993e963f6f8ed4afe5
rodrigofelixf <rodrigofelixferreira@hotmail.com>
2024-11-28 23:30:49 -0300

    Configura e organiza a documentacao do swegger para as rotas

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from app.controller.LoginController import routerlogin as api_router_login
 app = FastAPI()
 
 
-app.include_router(api_router, prefix="/v1/api")
-app.include_router(api_router_usuarios, prefix="/v1/api")
-app.include_router(api_router_login, prefix="/v1/api")
+app.include_router(api_router, prefix="/v1/api", tags=["Vazamentos"])
+app.include_router(api_router_usuarios, prefix="/v1/api", tags=["Usuarios"])
+app.include_router(api_router_login, prefix="/v1/api", tags=["Autenticacao"])
