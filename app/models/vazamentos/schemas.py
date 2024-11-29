d13c034b8ac0413fe984c5528fb9e8b160c8d1ac
rodrigofelixf <rodrigofelixferreira@hotmail.com>
2024-11-29 11:47:52 -0300

    atualizao response de vazamentos

--- app/models/vazamentos/schemas.py
+++ app/models/vazamentos/schemas.py
@@ -1,10 +1,37 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+class VazamentoBase(BaseModel):
+    nome: str
+    titulo: str
+    dominio_url: str
+    data_vazamento: datetime
+    data_atualizacao: datetime
+    descricao: Optional[str] = None
+    image_uri: Optional[str] = None
+
+class VazamentoCreate(BaseModel):
+    nome: str
+    titulo: str
+    dominio_url: str
+    data_vazamento: datetime
+    data_atualizacao: datetime
+    descricao: Optional[str] = None
+    image_uri: Optional[str] = None
+    usuario_id: int
 
 
-class VazamentoRequest(BaseModel):
-    email: str
-
-
-class VazamentoReponse(BaseModel):
+class VazamentoResponse(BaseModel):
     id: int
-    email: str
+    nome: str
+    titulo: str
+    dominio_url: str
+    data_vazamento: datetime
+    data_atualizacao: datetime
+    descricao: Optional[str] = None
+    image_uri: Optional[str] = None
+    usuario_id: int
+
+    class Config:
+        orm_mode = True
