430ceb115cb78826e0c04da4a3cdab701634ae3e
rodrigofelixf <rodrigofelixferreira@hotmail.com>
2024-11-28 23:23:37 -0300

    atualiza e implementar os controllers do login e usuario

--- app/controller/UsuarioController.py
+++ app/controller/UsuarioController.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, status
 from app.service import UsuarioService
 from app.models.usuarios import UsuarioModel, UsuarioSchemas
 from app.db.database import SessionLocal, engine
@@ -20,17 +20,24 @@ def get_db():
 
 
 
-@routerusuarios.get(endpointUsuario + "procurar/{usuarioId}", response_model= UsuarioSchemas.UsuarioReponse)
+@routerusuarios.get(endpointUsuario + "{usuarioId}", response_model= UsuarioSchemas.UsuarioReponse)
 def obter_usuario_por_id(usuarioId: int, db: Session= Depends(get_db)):
-
-    usuarioEncontrado = UsuarioService.get_usuarios(db, usuarioId)
-
-    if usuarioEncontrado is None:
-        raise HTTPException(status_code = 404, detail= "Usuario não encontrado")
-
-
+    usuarioEncontrado = UsuarioService.obter_usuario_pelo_id(db, usuarioId)
     return usuarioEncontrado
 
+@routerusuarios.get(endpointUsuario + "procurar/{usuarioEmail}", response_model= UsuarioSchemas.UsuarioReponse)
+def obter_usuario_por_email(usuarioEmail: str, db: Session= Depends(get_db)):
+
+    usuarioEncontrado = UsuarioService.obter_usuario_pelo_email(db, usuarioEmail)
+    return usuarioEncontrado
+
+
+@routerusuarios.post(endpointUsuario, response_model = UsuarioSchemas.UsuarioReponse, status_code=status.HTTP_201_CREATED)
+def criar_usuario(usuario: UsuarioSchemas.CreateUserRequest, db: Session= Depends(get_db)):
+
+    usuario = UsuarioService.criar_usuario(db, usuario)
+
+    return  usuario
 
 
 
